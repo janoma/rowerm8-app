554aedcf5ffe3aa3de6ff5615598c979ccd561d4
Alejandro Mallea <janoma@gmail.com>
2026-05-03 18:43:38 -0700

    replace default assets with app skeleton

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -26,8 +26,10 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Row',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="figure.indoor.rowing" color={color} />
+          ),
         }}
       />
     </Tabs>
